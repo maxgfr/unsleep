9e01e96654853bba5ad46db811c1861da00805ec
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-12-03 15:40:17 +0100

    feat: add pkg

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-export * from "./hello"
+import robot from 'robotjs';
+
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const randomIntFromInterval = (min: number, max: number) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+  
+const screenSize = robot.getScreenSize();
+
+const DELAY_MS = 5000;
+
+(async function () {
+  while (true) {
+    await delay(DELAY_MS);
+    robot.moveMouse(
+      randomIntFromInterval(0, screenSize.height),
+      randomIntFromInterval(0, screenSize.width),
+    );
+  }
+})();
